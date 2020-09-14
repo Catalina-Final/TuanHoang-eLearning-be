173c2a74cdb79f40bb204278087e6ba3f4c1c430
Luke Hoang <lukehoang195@gmail.com>
2020-09-14 16:26:34 +0700

    remove unsaved

--- src/models/student.js
+++ src/models/student.js
@@ -7,4 +7,6 @@ const studentSchema = Schema({
   user: { type: Schema.Types.ObjectId, require: "true", ref: "User" },
   isDeleted: { type: Boolean, default: false },
 });
+studentSchema.plugin(require("./plugins/isDeletedFalse"));
+
 module.exports = mongoose.model("Student", studentSchema);
